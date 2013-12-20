a436898f1d23b2ee2fb276863a844d186531036e
Ye Hua <mantedh@student.unimelb.edu.au>
2013-12-20 20:08:57 +0800

    add device detection

--- Game of Life/DefinedConstants.h
+++ Game of Life/DefinedConstants.h
@@ -8,12 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+#define IS_WIDESCREEN ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
+#define IS_IPHONE ( [ [ [ UIDevice currentDevice ] model ] isEqualToString: @"iPhone" ] )
+#define IS_IPOD   ( [ [ [ UIDevice currentDevice ] model ] isEqualToString: @"iPod touch" ] )
+#define IS_IPHONE_5 ( IS_IPHONE && IS_WIDESCREEN )
+
 #define ALIVE 0
 #define DEAD 1
 
-#define CELL_WIDTH 20
+#define CELL_WIDTH 10
 #define AREA_WIDTH 320
-#define AREA_HEIGHT 400
+#define AREA_HEIGHT (IS_WIDESCREEN?488:400)
 #define NUM_OF_ROW AREA_HEIGHT / CELL_WIDTH
 #define NUM_OF_COL AREA_WIDTH / CELL_WIDTH
 #define CELL_TAG 1
